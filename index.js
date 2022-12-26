53f76dfed2bec90a537cd448f3f1fa79c749c42d
mzncvmc <mzncvmc@gmail.com>
2022-12-26 21:29:41 +0900

    #7 BodyParser & PostMan & 회원 가입 기능

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = 5000;
 
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const uri = "mongodb+srv://mzncvmc:1234@boilerplate.nwwwnqa.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -22,4 +30,15 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
 });
