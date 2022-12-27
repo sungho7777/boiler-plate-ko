6591379bb9cbae49ff110c09b313a62ab4c86c8b
mzncvmc <mzncvmc@gmail.com>
2022-12-27 21:51:37 +0900

    #10 Bcrypt로 비밀번호 암호화

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 5000;
@@ -14,19 +14,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
 
-const uri = config.mongoURL;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const mongoURL = config.mongoURL;
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('MongoDB Connected...');
-
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.set("strictQuery", false);
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true
+    ,useUnifiedTopology: true
+    //,useCreateIndex: true
+    //,useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...!'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello nodemon !!!');
+  res.send('Hello nodemon !!!!');
 });
 
 app.listen(port, () => {
@@ -36,6 +36,7 @@ app.listen(port, () => {
 app.post('/register', (req, res) => {
     // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
     const user = new User(req.body);
+    
     user.save((err, userInfo) => {
         if (err) return res.json({success: false, err});
         return res.status(200).json({
