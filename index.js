31021a5aa43ef321610c0842ac9b0c0d13871a5b
mzncvmc <mzncvmc@gmail.com>
2022-12-27 22:10:43 +0900

    #11 로그인 기능 with Bcrypt (1)

--- index.js
+++ index.js
@@ -44,3 +44,29 @@ app.post('/register', (req, res) => {
         });
     });
 });
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+  User.findOne({email: req.body.emaul}, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess: true,
+        message: '제공된 이메일이 해당하는 유저가 없습니다.'
+      });
+    }
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({loginSuccess : false, massage:'비밀번호가 틀렸습니다.'});
+    });
+    // 비밀번호 까지 맞다면 토큰을 생성한다.
+    user.generateToken((err, user) => {
+      
+    });
+  });
+
+
+
+
+
+});
--- models/User.js
+++ models/User.js
@@ -45,8 +45,17 @@ userSchema.pre('save', function(next) {
                 next();
             });
         });
+    }else{
+        next();
     }
 });
+
+userSchema.methods.comparePasswprd = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err), cb(null, isMatch);
+        
+    });
+}
 
 const User = mongoose.model('User', userSchema);
 
