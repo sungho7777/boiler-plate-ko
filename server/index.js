fe5fa9406c08838a46c40e12536a22afecff66b3
mzncvmc <mzncvmc@gmail.com>
2022-12-29 21:21:06 +0900

    #21 Data Flow & Axios

--- server/index.js
+++ server/index.js
@@ -36,6 +36,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+app.get('/api/hello', (req, res) => {
+  res.send('안녕하세요~');
+});
+
 app.post('/api/users/register', (req, res) => {
     // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
     const user = new User(req.body);
